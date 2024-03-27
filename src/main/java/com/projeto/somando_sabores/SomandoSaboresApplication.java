20fc810f6cbde502a3f78ac64913a8e1acc99fb2
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-27 18:49:59 -0300

    first commit: inicialização padrão do spring

---
+++ src/main/java/com/projeto/somando_sabores/SomandoSaboresApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.somando_sabores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomandoSaboresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomandoSaboresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/somando_sabores/SomandoSaboresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.somando_sabores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomandoSaboresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
