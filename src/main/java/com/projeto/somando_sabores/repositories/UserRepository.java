813c4a8197e32544490c0b9dfe9992471c8ce8a4
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-30 16:47:11 -0300

    Criado repositorio, UserRepository.java

---
+++ src/main/java/com/projeto/somando_sabores/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.somando_sabores.repositories;
+
+import com.projeto.somando_sabores.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
