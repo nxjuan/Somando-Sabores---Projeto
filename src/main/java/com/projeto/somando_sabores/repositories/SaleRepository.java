514e7d7b20d5492f75272037ff0c54b6594388d3
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-30 16:50:49 -0300

    Criado Repository, SaleRepository.java

---
+++ src/main/java/com/projeto/somando_sabores/repositories/SaleRepository.java
@@ -0,0 +1,12 @@
+package com.projeto.somando_sabores.repositories;
+
+import com.projeto.somando_sabores.models.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<Sale> findByUser_Id(Long id);
+}
