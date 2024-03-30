1a4e61d93a26327c1d35733cdac36ae4f84e6a6f
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-30 16:18:04 -0300

    Criada entidade Sale

---
+++ src/main/java/com/projeto/somando_sabores/models/Sale.java
@@ -0,0 +1,23 @@
+package com.projeto.somando_sabores.models;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "sales")
+public class Sale implements Serializable{
+    @Id
+    @Column(name="id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name="date")
+    @NotBlank
+    private Instant date;
+    @Column(name="value")
+    @NotBlank
+    private double value;
+
+
+}
