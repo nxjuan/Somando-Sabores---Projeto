4e7807e79b49b22450403829a95e03366b8c6024
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-31 17:32:37 -0300

    ProductRepository.java updated, a função encontra os produtos a partir do id da venda

--- src/main/java/com/projeto/somando_sabores/repositories/ProductRepository.java
+++ src/main/java/com/projeto/somando_sabores/repositories/ProductRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findProduct_Id(Long id);
+    Product findBySale_Id(Long id);
 
 }
