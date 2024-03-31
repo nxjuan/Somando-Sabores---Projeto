e17dd3074ae067a9e6a9967b5abd1e85be70a2cf
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-31 15:09:28 -0300

    Criado service, SaleService.java

---
+++ src/main/java/com/projeto/somando_sabores/services/SaleService.java
@@ -0,0 +1,53 @@
+package com.projeto.somando_sabores.services;
+
+import com.projeto.somando_sabores.models.Sale;
+import com.projeto.somando_sabores.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public Sale findById(Long id){
+        Optional<Sale> sale = this.saleRepository.findById(id);
+        return sale.orElseThrow(
+                (
+                        () -> new RuntimeException(
+                            "Venda não encontrada! id: " + id + ", tipo: " + Sale.class.getName()
+                        )
+                )
+        );
+    }
+    @Transactional
+    public Sale create(Sale sale){
+        sale.setId(null);
+        sale = this.saleRepository.save(sale);
+        return sale;
+    }
+
+    @Transactional
+    public Sale update(Sale sale){
+        Sale newSale = findById(sale.getId());
+        newSale.setValue(sale.getValue());
+        newSale.setProduct(sale.getProduct());
+        return this.saleRepository.save(newSale);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try{
+            this.saleRepository.deleteById(id);
+        }
+        catch(Exception e){
+            throw new RuntimeException(
+                    "Não é possivel excluir pois há entidades relacionadas!"
+            );
+        }
+    }
+}
