404777dfc548872f2f35a62723f4900163532c29
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-03-31 23:28:06 -0300

    Controller criado, UserController.java

---
+++ src/main/java/com/projeto/somando_sabores/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.projeto.somando_sabores.controllers;
+
+import com.projeto.somando_sabores.models.User;
+import com.projeto.somando_sabores.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = this.userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody User user){
+        this.userService.create(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody User user, @PathVariable Long id){
+        user.setId(id);
+        this.userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
