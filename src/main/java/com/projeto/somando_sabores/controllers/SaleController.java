87fc7d2dc8823dc0b7cc63c4131391d4b269247f
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-04-10 16:27:10 -0300

    SaleController updated, findUserBySaleId

--- src/main/java/com/projeto/somando_sabores/controllers/SaleController.java
+++ src/main/java/com/projeto/somando_sabores/controllers/SaleController.java
@@ -1,6 +1,7 @@
 package com.projeto.somando_sabores.controllers;
 
 import com.projeto.somando_sabores.models.Sale;
+import com.projeto.somando_sabores.repositories.SaleRepository;
 import com.projeto.somando_sabores.services.SaleService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sale")
@@ -19,10 +21,19 @@ public class SaleController {
     @Autowired
     private SaleService saleService;
 
+    @Autowired
+    private SaleRepository saleRepository;
+
     @GetMapping("/{id}")
     public ResponseEntity<Sale> findById(@PathVariable Long id){
         Sale sale = this.saleService.findById(id);
         return ResponseEntity.ok(sale);
+    }
+
+    @GetMapping("/user/{userId}")
+    private ResponseEntity<List<Sale>> findAllByUserId(Long userId){
+        List<Sale> sales = this.saleRepository.findByUser_Id(userId);
+        return ResponseEntity.ok().body(sales);
     }
 
     @PostMapping
