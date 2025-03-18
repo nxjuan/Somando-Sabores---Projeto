6fedcc8dc7bbb7dcbc334bd83b72d0f2f4dabc55
Juan Nogueira <juannogueiraxavier@gmaail.com>
2025-03-18 17:37:38 -0300

    paginas do header inicializadas

--- angular-interface/src/app/pages/pacotes/pacotes.component.ts
+++ angular-interface/src/app/pages/pacotes/pacotes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderBarComponent } from '../../components/header-bar/header-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-pacotes',
-  imports: [HeaderBarComponent],
+  imports: [HeaderBarComponent, RouterModule],
   templateUrl: './pacotes.component.html',
   styleUrl: './pacotes.component.scss'
 })
