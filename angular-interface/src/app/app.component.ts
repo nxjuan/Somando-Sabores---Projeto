d75548e96c970dee9bec462a0bff7aebdf589f02
Juan Nogueira <juannogueiraxavier@gmaail.com>
2025-03-18 17:36:36 -0300

    configurações de rota do projeto

--- angular-interface/src/app/app.component.ts
+++ angular-interface/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angular-interface/src/app/app.routes.ts
+++ angular-interface/src/app/app.routes.ts
@@ -1,8 +1,18 @@
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { CardapioComponent } from './pages/cardapio/cardapio.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PacotesComponent } from './pages/pacotes/pacotes.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'cardapio', component: CardapioComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'pacotes', component: PacotesComponent },
+  { path: 'reservas', component: ReservasComponent },
 
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
 ];
