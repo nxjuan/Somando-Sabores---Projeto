0dff7fb772711687f418e2590f0655e4f8bcf4f1
Juan Nogueira <juannogueiraxavier@gmaail.com>
2025-03-18 17:33:14 -0300

    rotas configuradas no header

--- angular-interface/src/app/components/header-bar/header-bar.component.ts
+++ angular-interface/src/app/components/header-bar/header-bar.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header-bar',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.scss'
 })
 export class HeaderBarComponent {
   imageUrl:string = "src/assets/images/Logo-3.png";
+
+  constructor(private route: Router) { }
+
+  mudaPagina(rota: any){
+    this.route.navigate([`/${rota}`])
+  }
 }
