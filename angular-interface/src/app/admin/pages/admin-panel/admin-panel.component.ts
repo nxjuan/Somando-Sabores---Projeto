647d98a9f2817b71fbd165bcb77261efb846a0ee
MicheleMelo0 <michele_melo@educadventista.org>
2025-05-28 12:06:59 -0400

    add: tela de login do administrador

--- angular-interface/src/app/admin/pages/admin-panel/admin-panel.component.ts
+++ angular-interface/src/app/admin/pages/admin-panel/admin-panel.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-admin-panel',
-  imports: [],
+  imports: [MatIcon, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.scss'
 })
