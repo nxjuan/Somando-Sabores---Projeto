964b8ed6e5b126f7fb46f96a49e4af184a2591f7
Bruno Ferreira <100526804+Bruno441@users.noreply.github.com>
2025-06-11 23:42:14 -0300

    Teste de build 2

--- angular-interface/src/app/test.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-test',
-  standalone: true,
-  template: '<h1>Build Funcionou! A aplicação completa é pesada demais.</h1>'
-})
-export class TestComponent {}
