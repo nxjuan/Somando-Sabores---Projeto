22daa624edcce4544743f7c7a6f73d7bcd188edf
Bruno Ferreira <100526804+Bruno441@users.noreply.github.com>
2025-06-11 23:16:03 -0300

    teste de build

--- angular-interface/src/app/app.routes.ts
+++ angular-interface/src/app/app.routes.ts
@@ -1,30 +1,37 @@
-import { EventosComponent } from './pages/eventos/eventos.component';
-import { CardapioComponent } from './pages/cardapio/cardapio.component';
+// import { EventosComponent } from './pages/eventos/eventos.component';
+// import { CardapioComponent } from './pages/cardapio/cardapio.component';
+// import { Routes } from '@angular/router';
+// import { HomeComponent } from './pages/home/home.component';
+// import { PacotesComponent } from './pages/pacotes/pacotes.component';
+// import { ReservasComponent } from './pages/reservas/reservas.component';
+// import { AdminPanelComponent } from './admin/pages/admin-panel/admin-panel.component';
+// import { AdmRelatoriosComponent } from './admin/pages/adm-relatorios/adm-relatorios.component';
+// import { AdmReservasComponent } from './admin/pages/adm-reservas/adm-reservas.component';
+// import { AdmPacotesComponent } from './admin/pages/adm-pacotes/adm-pacotes.component';
+// import { AdmPagamentosComponent } from './admin/pages/adm-pagamentos/adm-pagamentos.component';
+// import { ConfirmacaoComponent } from './pages/confirmacao/confirmacao.component';
+// import { ResumoPacoteComponent } from './pages/resumo-pacote/resumo-pacote.component';
+
+// export const routes: Routes = [
+//   { path: 'home', component: HomeComponent },
+//   { path: 'cardapio', component: CardapioComponent },
+//   { path: 'eventos', component: EventosComponent },
+//   { path: 'pacotes', component: PacotesComponent },
+//   { path: 'reservas', component: ReservasComponent },
+//   { path: 'admin-panel', component: AdminPanelComponent},
+//   { path: 'admin-panel/relatorios', component: AdmRelatoriosComponent},
+//   { path: 'admin-panel/reservas', component: AdmReservasComponent},
+//   { path: 'admin-panel/pacotes', component: AdmPacotesComponent},
+//   { path: 'admin-panel/pagamentos', component: AdmPagamentosComponent},
+//   { path: 'confirmacao', component: ConfirmacaoComponent },
+//   { path: 'resumo-pacote', component: ResumoPacoteComponent },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: '**', component: HomeComponent }
+// ];
+
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { PacotesComponent } from './pages/pacotes/pacotes.component';
-import { ReservasComponent } from './pages/reservas/reservas.component';
-import { AdminPanelComponent } from './admin/pages/admin-panel/admin-panel.component';
-import { AdmRelatoriosComponent } from './admin/pages/adm-relatorios/adm-relatorios.component';
-import { AdmReservasComponent } from './admin/pages/adm-reservas/adm-reservas.component';
-import { AdmPacotesComponent } from './admin/pages/adm-pacotes/adm-pacotes.component';
-import { AdmPagamentosComponent } from './admin/pages/adm-pagamentos/adm-pagamentos.component';
-import { ConfirmacaoComponent } from './pages/confirmacao/confirmacao.component';
-import { ResumoPacoteComponent } from './pages/resumo-pacote/resumo-pacote.component';
+import { TestComponent } from './test.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'cardapio', component: CardapioComponent },
-  { path: 'eventos', component: EventosComponent },
-  { path: 'pacotes', component: PacotesComponent },
-  { path: 'reservas', component: ReservasComponent },
-  { path: 'admin-panel', component: AdminPanelComponent},
-  { path: 'admin-panel/relatorios', component: AdmRelatoriosComponent},
-  { path: 'admin-panel/reservas', component: AdmReservasComponent},
-  { path: 'admin-panel/pacotes', component: AdmPacotesComponent},
-  { path: 'admin-panel/pagamentos', component: AdmPagamentosComponent},
-  { path: 'confirmacao', component: ConfirmacaoComponent },
-  { path: 'resumo-pacote', component: ResumoPacoteComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: HomeComponent }
+  { path: '', component: TestComponent }
 ];
---
+++ angular-interface/src/app/test.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  template: '<h1>Build Funcionou! A aplicação completa é pesada demais.</h1>'
+})
+export class TestComponent {}
