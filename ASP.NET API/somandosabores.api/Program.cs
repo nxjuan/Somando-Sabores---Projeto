87db5ee1dea3196ee0f83e799a34e0d35415db21
MicheleMelo0 <134606859+MicheleMelo0@users.noreply.github.com>
2025-06-12 14:23:43 -0300

    test: expose swagger

--- ASP.NET API/somandosabores.api/Program.cs
+++ ASP.NET API/somandosabores.api/Program.cs
@@ -45,15 +45,12 @@ builder.Services.AddScoped<IPrecificacaoService, PrecificacaoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
         c.RoutePrefix = string.Empty;
     });
-}
 
 app.UseCors("AllowSpecificOrigin");
 
